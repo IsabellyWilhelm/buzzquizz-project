b29eb37a3ae1506421cf54adf6cfaecf1d5ed487
sabta <sabtaamendes@gmail.com>
2022-06-30 00:50:22 -0400

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,30 @@
 // buscar imagens
-let urlQuizz = "https://mock-api.driven.com.br/api/v4/buzzquizz";
+//let urlQuizz = "https://mock-api.driven.com.br/api/v4/buzzquizz";
+
 getImage();
 function getImage(){
-    const promisse = axios.get(`${urlQuizz}/image`);
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promisse.then(showImage);
+    console.log(promisse)
 }
 
 function showImage(response){
-    const 
+    console.log(response)
+    let quizzImage = document.querySelector(".local-todos-quizzes")
+    quizzImage.innerHTML="";
+    
+    for(let i = 0; response.data.length; i++){
+        quizzImage +=`
+        <section class="local-todos-quizzes hidden">
+            <h2>Todos os Quizzes</h2>
+            <ul class="todos-quizzes">
+                <li>
+                    <img src="${response.data[i].image}">
+                    <p>${response.data[i].text}</p>
+                </li>
+            </ul>
+        </section> 
+     `
+    }
 }
+
