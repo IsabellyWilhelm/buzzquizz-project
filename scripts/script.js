83c1182715461bd92e66b290ea92f5ee7cc475f9
sabta <sabtaamendes@gmail.com>
2022-06-30 01:25:20 -0400

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // buscar imagens
 //let urlQuizz = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 
-getImage();
+/*getImage();
 function getImage(){
     const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promisse.then(showImage);
@@ -26,5 +26,91 @@ function showImage(response){
         </section> 
      `
     }
+}*/
+let arrayQuizzesServidor;
+function getQuizzes() {
+    const quizzes = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
+    quizzes.then(function (resposta) {
+        arrayQuizzesServidor = resposta.data;
+        preencheContainerDeQuiz(".todos-quizzes", arrayQuizzesServidor)
+        console.log("quizzes")
+    });
 }
 
+
+function quizzes(){
+    quizzes.catch(window.location.reload);
+    arrayQuizzesServidor;
+}
+   
+[
+	{
+		id: 1,
+		title: "Título do quizz",
+		image: "https://http.cat/411.jpg",
+		questions: [
+			{
+				title: "Título da pergunta 1",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			},
+			{
+				title: "Título da pergunta 2",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			},
+			{
+				title: "Título da pergunta 3",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			}
+		],
+		levels: [
+			{
+				title: "Título do nível 1",
+				image: "https://http.cat/411.jpg",
+				text: "Descrição do nível 1",
+				minValue: 0
+			},
+			{
+				title: "Título do nível 2",
+				image: "https://http.cat/412.jpg",
+				text: "Descrição do nível 2",
+				minValue: 50
+			}
+		]
+	}
+]
