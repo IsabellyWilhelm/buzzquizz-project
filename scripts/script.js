179334c3d62fbdb5c54f59211f01ae35927a9a6f
sabta <sabtaamendes@gmail.com>
2022-06-30 23:23:12 -0400

    js

--- scripts/script.js
+++ scripts/script.js
@@ -2,52 +2,60 @@
 //let urlQuizz = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 
 getImage();
-function getImage(){
-    const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    promisse.then(showImage);
-    console.log(promisse)
+function getImage() {
+	const promisse = axios.get("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes");
+	promisse.then(showImage);
+	console.log(promisse)
 }
 
-function showImage(response){
-    console.log(response)
-    let quizzImage = document.querySelector(".local-todos-quizzes")
-    quizzImage.innerHTML="";
-    
-    for(let i = 0; response.data.length; i++){
-        quizzImage +=`
+function showImage(response) {
+	console.log(response)
+	let quizzImage = document.querySelector(".todos-quizzes")
+	quizzImage.innerHTML = "";
+
+	for (let i = 0; response.data.length; i++) {
+		quizzImage += `
         <section class="local-todos-quizzes hidden">
             <h2>Todos os Quizzes</h2>
             <ul class="todos-quizzes">
                 <li>
-                    <img src="${response.data[i].image}">
-                    <p>${response.data[i].text}</p>
+                    <img src="${response.data[i].image}.png">
+                    <p>${response.data[i].title}</p>
                 </li>
             </ul>
         </section> 
      `
-    }
+	}
 }
+AllQuizzezz();
+function AllQuizzezz(){
+	const promisse = axios.get("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes");
+	promisse.then(getAllQuizzezz);
+	console.log(promisse)
+}
+function getAllQuizzezz(response) {
+	const elementImage = document.querySelector(".todos-quizzes");
+	const elementText = document.querySelector(".todos-quizzes");
 
-
-function getAllQuizzezz(){
-	const elementImage = document.querySelector(".local-todos-quizzes");
-	const elementText =document.querySelector(".")
+	elementImage.src = response.data.image;
+	elementText.innerHTML = response.data.title;
+	console.log(response)
 }
 
 /*let arrayQuizzesServidor;
 function getQuizzes() {
-    const quizzes = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
-    quizzes.then(function (resposta) {
-        arrayQuizzesServidor = resposta.data;
-        preencheContainerDeQuiz(".todos-quizzes", arrayQuizzesServidor)
-        console.log("quizzes")
-    });
+	const quizzes = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
+	quizzes.then(function (resposta) {
+		arrayQuizzesServidor = resposta.data;
+		preencheContainerDeQuiz(".todos-quizzes", arrayQuizzesServidor)
+		console.log("quizzes")
+	});
 }
 
 
 function quizzes(){
-    quizzes.catch(window.location.reload);
-    arrayQuizzesServidor;
+	quizzes.catch(window.location.reload);
+	arrayQuizzesServidor;
 }
    
 [
@@ -127,29 +135,29 @@ let arrayQuizzesServidor;
 
 //Busca todos os quizzes no Servidor, e armazena o  array obtido na variável global "arrayQuizzesServidor"
 function getQuizzes() {
-    const quizzes = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
-    quizzes.then(function(resposta) {
-        arrayQuizzesServidor = resposta.data;
-        preencheContainerDeQuiz(".todos-quizzes",arrayQuizzesServidor)
-        console.log("Sucesso")
-    });
-    quizzes.catch(window.location.reload);
+	const quizzes = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
+	quizzes.then(function (resposta) {
+		arrayQuizzesServidor = resposta.data;
+		preencheContainerDeQuiz(".todos-quizzes", arrayQuizzesServidor)
+		console.log("Sucesso")
+	});
+	quizzes.catch(window.location.reload);
 }
 
 
 function preencheContainerDeQuiz(classeDoContainer, arrayDeQuizzes) {
-    let localTodos = document.querySelector(".todos-quizzes");
-    let localSeus = document.querySelector(".seus-quizzes");
-    let meusQuizzesId = [];
-    localSeus.innerHTML = "";
-    localTodos.innerHTML = "";
-    if(localStorage.getItem("idsLocal") !== null) {
-        meusQuizzesId = JSON.parse(localStorage.getItem("idsLocal"));
-    }
+	let localTodos = document.querySelector(".todos-quizzes");
+	let localSeus = document.querySelector(".seus-quizzes");
+	let meusQuizzesId = [];
+	localSeus.innerHTML = "";
+	localTodos.innerHTML = "";
+	if (localStorage.getItem("idsLocal") !== null) {
+		meusQuizzesId = JSON.parse(localStorage.getItem("idsLocal"));
+	}
 
-    for(let i=0;i<arrayDeQuizzes.length;i++) {
-        if(meusQuizzesId.includes(arrayDeQuizzes[i].id)){
-            localSeus.innerHTML += `<div class="card">
+	for (let i = 0; i < arrayDeQuizzes.length; i++) {
+		if (meusQuizzesId.includes(arrayDeQuizzes[i].id)) {
+			localSeus.innerHTML += `<div class="card">
                                     <li class="quizz" id="${arrayDeQuizzes[i].id}" onclick="executaQuiz(${arrayDeQuizzes[i].id})">
                                         <img src="${arrayDeQuizzes[i].image}">
                                         <div class="gradient"></div>
@@ -157,13 +165,13 @@ function preencheContainerDeQuiz(classeDoContainer, arrayDeQuizzes) {
                                     </li>
                                     <div class="delete" onclick="remove(${arrayDeQuizzes[i].id})"><ion-icon name="trash-outline"></ion-icon></div>
                                     </div>`;
-        }else {
-        localTodos.innerHTML += `<li class="quizz" id="${arrayDeQuizzes[i].id}" onclick="executaQuiz(${arrayDeQuizzes[i].id})">
+		} else {
+			localTodos.innerHTML += `<li class="quizz" id="${arrayDeQuizzes[i].id}" onclick="executaQuiz(${arrayDeQuizzes[i].id})">
                                     <img src="${arrayDeQuizzes[i].image}">
                                     <div class="gradient"></div>
                                     <h3>${arrayDeQuizzes[i].title}</h3>
                                 </li>`;
-        }
-    }
-    trocaDeSection();
+		}
+	}
+	trocaDeSection();
 }
